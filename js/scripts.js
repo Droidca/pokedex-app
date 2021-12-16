95a21a8def94bea9c2279020817b0bfb06dbbbb3
Droidca <david-agostinho@hotmail.com>
2021-12-16 17:17:31 +0000

    Implement for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
     let pokemonList = [
-      {name: 'Bulbasaur', height:'0.7', type: ['grass', 'poison']}
-      {name: 'Charmander', height:'0.6', type: 'fire'}
-      {name: 'Squirtle', height:'0.5', type: 'water'}
-    ]
+      {name: 'Bulbasaur', height:'0.7', type: ['Grass', 'Poison']},
+      {name: 'Charmander', height:'0.6', type: 'Fire'},
+      {name: 'Squirtle', height:'0.5', type: 'Water'}
+    ];
+
+    for (let i = 0; i < pokemonList.length; i++) {
+      document.write(pokemonList[i].name + " " + "(Height: " + pokemonList[i].height
+       + ", Type: " + pokemonList[i].type + ") ");
+    }
