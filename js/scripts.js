91c8771575697090154388ea678687b554d005e9
Droidca <david-agostinho@hotmail.com>
2021-12-30 18:34:07 +0000

    Add IIFE for pokemonList Array

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,93 @@
 //Pokemon atributes for pokédex
 
+let pokemonRepository = (function () {
     let pokemonList = [
-      {
-        number: '#001',
-        name: 'Bulbasaur',
-        height: 0.7,
-        type: ['Grass', 'Poison']
-      },
-      {
-        number: '#002',
-        name: 'Ivysaur',
-        height: 1.0,
-        type: ['Grass', 'Poison']
-      },
-      {
-        number: '#003',
-        name: 'Venusaur',
-        height: 2.0,
-        type: ['Grass', 'Poison']
-      },
-      {
-        number: '#004',
-        name: 'Charmander',
-        height: 0.6,
-        type: 'Fire'
-      },
-      {
-        number: '#005',
-        name: 'Charmeleon',
-        height: 1.1,
-        type: 'Fire'
-      },
-      {
-        number: '#006',
-        name: 'Charizard',
-        height: 1.7,
-        type: ['Fire', 'Flying']
-      },
-      {
-        number: '#007',
-        name: 'Squirtle',
-        height: 0.5,
-        type: 'Water'
-      },
-      {
-        number: '#008',
-        name: 'Wartortle',
-        height: 1.0,
-        type: 'Water'
-      },
-      {
-        number: '#009',
-        name: 'Blastoise',
-        height: 1.6,
-        type: 'Water'
-      }
+        {
+          number: '#001',
+          name: 'Bulbasaur',
+          height: 0.7,
+          type: ['Grass', 'Poison']
+        },
+        {
+          number: '#002',
+          name: 'Ivysaur',
+          height: 1.0,
+          type: ['Grass', 'Poison']
+        },
+        {
+          number: '#003',
+          name: 'Venusaur',
+          height: 2.0,
+          type: ['Grass', 'Poison']
+        },
+        {
+          number: '#004',
+          name: 'Charmander',
+          height: 0.6,
+          type: 'Fire'
+        },
+        {
+          number: '#005',
+          name: 'Charmeleon',
+          height: 1.1,
+          type: 'Fire'
+        },
+        {
+          number: '#006',
+          name: 'Charizard',
+          height: 1.7,
+          type: ['Fire', 'Flying']
+        },
+        {
+          number: '#007',
+          name: 'Squirtle',
+          height: 0.5,
+          type: 'Water'
+        },
+        {
+          number: '#008',
+          name: 'Wartortle',
+          height: 1.0,
+          type: 'Water'
+        },
+        {
+          number: '#009',
+          name: 'Blastoise',
+          height: 1.6,
+          type: 'Water'
+        }
     ];
 
-    //this loop alows the pokémon atributes to be displayed on the page.
+    function getAll() {
+        return pokemonList;
+    }
 
-    //if condition added, so there is an extra comment for a bigger pokémon.
+    function add(newPokemon) {
+        pokemonList.push(newPokemon);
+    }
 
-    //added <br> for line breaks, so the information gets properly displayed.
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
 
-    /*for (let i = 0; i < pokemonList.length; i++) {
-      document.write(pokemonList[i].name + " " + "(Height: "
-      + pokemonList[i].height + ", Type: " + pokemonList[i].type + ") ");
-      if(pokemonList[i].height > 1.5){
-        document.write('That pokemon is huge!');
+  //this loop alows the pokémon atributes to be displayed on the page.
+
+  //if condition added, so there is an extra comment for a bigger pokémon.
+
+function myPokedex(list) {
+  list.forEach(function(pokemon) {
+      if (pokemon.height >= 1.5) {
+          document.write('<p>' + pokemon.number + ' ' + pokemon.name + ', height: '
+          + pokemon.height + ' ft, types: ' + pokemon.type + ' (Wow! That is huge!)' + '</p>');
       }
-      document.write('<br>');
-    }*/
-
-    pokemonList.forEach(function(pokemon) {
+      else {
         document.write('<p>' + pokemon.number + ' ' + pokemon.name + ', height: '
-        + pokemon.height + ' ft, types: ' + pokemon.type + '</p>')
-    });
+        + pokemon.height + ' ft, types: ' + pokemon.type + '</p>');
+      }
+  });
+}
+
+
+myPokedex(pokemonRepository.getAll());
